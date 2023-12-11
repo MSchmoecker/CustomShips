76fcd8ed9a81395a45a00e8d85dd027600533ca5
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-12-11 16:53:19 +0100

    fix: rudder position update

--- CustomShips/Main.cs
+++ CustomShips/Main.cs
@@ -16,7 +16,7 @@ namespace CustomShips {
     public class Main : BaseUnityPlugin {
         public const string PluginGUID = "com.maxsch.valheim.CustomShips";
         public const string PluginName = "CustomShips";
-        public const string PluginVersion = "0.0.1";
+        public const string PluginVersion = "0.0.2";
 
         private static AssetBundle assetBundle;
         private static List<CustomPiece> pieces = new List<CustomPiece>();
