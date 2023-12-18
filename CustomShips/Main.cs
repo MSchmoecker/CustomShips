84726bccdabbb55454510e9bed55b053fe315fb3
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-12-18 21:32:52 +0100

    feat: added recipes

--- CustomShips/Main.cs
+++ CustomShips/Main.cs
@@ -37,30 +37,30 @@ namespace CustomShips {
             shipPrefab = assetBundle.LoadAsset<GameObject>("MS_CustomShip");
             PrefabManager.Instance.AddPrefab(shipPrefab);
 
-            AddShipPiece("MS_Keel_2m");
-            AddShipPiece("MS_Keel_4m");
-            AddShipPiece("MS_Keel_Bow_1");
-            AddShipPiece("MS_Keel_Bow_2");
-            AddShipPiece("MS_Rib_1.0m");
-            AddShipPiece("MS_Rib_1.2m");
-            AddShipPiece("MS_Rib_1.4m");
-            AddShipPiece("MS_Rib_1.6m");
-            AddShipPiece("MS_Rib_1.8m");
-            AddShipPiece("MS_Rib_2.0m");
-            AddShipPiece("MS_Rib_2.2m");
-            AddShipPiece("MS_Rib_2.4m");
-            AddShipPiece("MS_Rib_2.6m");
-            AddShipPiece("MS_Hull_Dynamic");
-            AddShipPiece("MS_Rudder_1");
-            AddShipPiece("MS_Crate_1");
-            AddShipPiece("MS_Barrel_1");
-            AddShipPiece("MS_Ladder_1");
-            AddShipPiece("MS_Sail_2_White");
-            AddShipPiece("MS_Sail_2_Red_1");
-            AddShipPiece("MS_Sail_2_Hide_1");
-            AddShipPiece("MS_Sail_1_White");
-            AddShipPiece("MS_Sail_1_Red_1");
-            AddShipPiece("MS_Sail_1_Hide_1");
+            AddShipPiece("MS_Keel_2m", "RoundLog", 4, "BronzeNails", 2);
+            AddShipPiece("MS_Keel_4m", "RoundLog", 8, "BronzeNails", 4);
+            AddShipPiece("MS_Keel_Bow_1", "RoundLog", 6, "BronzeNails", 4);
+            AddShipPiece("MS_Keel_Bow_2", "RoundLog", 4, "BronzeNails", 2);
+            AddShipPiece("MS_Rib_1.0m", "RoundLog", 4, "BronzeNails", 2);
+            AddShipPiece("MS_Rib_1.2m", "RoundLog", 4, "BronzeNails", 2);
+            AddShipPiece("MS_Rib_1.4m", "RoundLog", 5, "BronzeNails", 2);
+            AddShipPiece("MS_Rib_1.6m", "RoundLog", 5, "BronzeNails", 2);
+            AddShipPiece("MS_Rib_1.8m", "RoundLog", 6, "BronzeNails", 2);
+            AddShipPiece("MS_Rib_2.0m", "RoundLog", 6, "BronzeNails", 2);
+            AddShipPiece("MS_Rib_2.2m", "RoundLog", 7, "BronzeNails", 2);
+            AddShipPiece("MS_Rib_2.4m", "RoundLog", 7, "BronzeNails", 2);
+            AddShipPiece("MS_Rib_2.6m", "RoundLog", 8, "BronzeNails", 2);
+            AddShipPiece("MS_Hull_Dynamic", "Wood", 6, "BronzeNails", 2, "Resin", 10);
+            AddShipPiece("MS_Rudder_1", "Wood", 12, "BronzeNails", 8);
+            AddShipPiece("MS_Crate_1", "FineWood", 8, "BronzeNails", 6);
+            AddShipPiece("MS_Barrel_1", "FineWood", 8, "BronzeNails", 6);
+            AddShipPiece("MS_Ladder_1", "Wood", 4, "BronzeNails", 2);
+            AddShipPiece("MS_Sail_2_White", "FineWood", 20, "LeatherScraps", 12, "Resin", 8);
+            AddShipPiece("MS_Sail_2_Red_1", "FineWood", 20, "LeatherScraps", 12, "Resin", 8, "Raspberry", 4);
+            AddShipPiece("MS_Sail_2_Hide_1", "FineWood", 20, "DeerHide", 12, "Resin", 8);
+            AddShipPiece("MS_Sail_1_White", "FineWood", 30, "LeatherScraps", 20, "Resin", 12);
+            AddShipPiece("MS_Sail_1_Red_1", "FineWood", 30, "LeatherScraps", 20, "Resin", 12, "Raspberry", 8);
+            AddShipPiece("MS_Sail_1_Hide_1", "FineWood", 30, "DeerHide", 20, "Resin", 12);
 
             PieceManager.OnPiecesRegistered += OnPiecesRegistered;
         }
@@ -82,8 +82,8 @@ namespace CustomShips {
             }
         }
 
-        private void AddShipPiece(string pieceName) {
-            CustomPiece piece = new CustomPiece(assetBundle, pieceName, true, ShipPartConfig());
+        private void AddShipPiece(string pieceName, string ingredient1 = "", int amount1 = 0, string ingredient2 = "", int amount2 = 0, string ingredient3 = "", int amount3 = 0, string ingredient4 = "", int amount4 = 0) {
+            CustomPiece piece = new CustomPiece(assetBundle, pieceName, true, ShipPartConfig(ingredient1, amount1, ingredient2, amount2, ingredient3, amount3, ingredient4, amount4));
             PieceManager.Instance.AddPiece(piece);
 
             if (piece.PiecePrefab.TryGetComponent(out DynamicHull dynamicHull)) {
@@ -102,11 +102,21 @@ namespace CustomShips {
             return shipPrefab.name == Utils.GetPrefabName(piece);
         }
 
-        private PieceConfig ShipPartConfig() {
+        private PieceConfig ShipPartConfig(string ingredient1, int amount1, string ingredient2, int amount2, string ingredient3, int amount3, string ingredient4, int amount4) {
             PieceConfig stackConfig = new PieceConfig();
             stackConfig.PieceTable = PieceTables.Hammer;
+            stackConfig.CraftingStation = CraftingStations.Workbench;
             stackConfig.Category = "Ship";
-            stackConfig.AddRequirement(new RequirementConfig("Wood", 3, 0, true));
+
+            stackConfig.AddRequirement(new RequirementConfig(ingredient1, amount1, 0, true));
+            stackConfig.AddRequirement(new RequirementConfig(ingredient2, amount2, 0, true));
+            stackConfig.AddRequirement(new RequirementConfig(ingredient3, amount3, 0, true));
+            stackConfig.AddRequirement(new RequirementConfig(ingredient4, amount4, 0, true));
+
+            if (stackConfig.Requirements.Length == 0) {
+                stackConfig.AddRequirement(new RequirementConfig("Wood", 3, 0, true));
+            }
+
             return stackConfig;
         }
     }
